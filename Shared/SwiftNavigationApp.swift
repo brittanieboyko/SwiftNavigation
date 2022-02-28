788762d7d4e610099ad9fce81f37b83ca9b78117
Brittanie <brittanie.boyko@gmail.com>
2022-02-27 22:15:07 -0500

    Initial Commit

---
+++ Shared/SwiftNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftNavigationApp.swift
+//  Shared
+//
+//  Created by Brittanie Boyko on 2/27/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
